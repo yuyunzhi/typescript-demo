6f27dd37071c1e7293d3eed9542c2ce1075b199b
lambert <635429583@qq.com>
2018-08-16 16:19:56 +0800

    return 类型

---
+++ 3.ts
@@ -0,0 +1,12 @@
+{
+    function min(a:number, b:number): number {
+        if (a < b) {
+          return a;
+        } else {
+          return b;
+        }
+      }
+      
+      var c = min(1, 2);
+      console.log(c);
+}
