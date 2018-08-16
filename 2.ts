c0b907d97aab813f2dc485a1f4de46420775115a
lambert <635429583@qq.com>
2018-08-16 16:16:40 +0800

    多种类型参数

---
+++ 2.ts
@@ -0,0 +1,9 @@
+{
+    function add(a: string, b: string): string;
+    function add(a: number, b: number): number;
+    function add(a: any, b: any): any {
+        return a + b;
+    }
+
+    console.log(add('frank', 'lambert'))
+}
